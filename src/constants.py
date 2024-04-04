1a1eadc9d1f3548a20c04d5ddf465b9a3a5ca0d7
Ivan Zakazov <kechua98@gmail.com>
2024-04-04 11:58:30 +0200

    FEAT: gpt-4 price added to constants

--- src/constants.py
+++ src/constants.py
@@ -8,6 +8,8 @@ class Models(Enum):
     TURBO_3_5 = "gpt-3.5-turbo"
     TURBO_3_5_INSTRUCT = "gpt-3.5-turbo-instruct"
     AZURE_3_5_TURBO = "gpt-35-turbo-0125"
+    AZURE_4_TURBO = "gpt-4-0125-Preview"
+    AZURE_4 = "gpt-4-0613"
 
 """The costs of the models above (per million tokens)."""
 MODELS_COST = {
@@ -22,5 +24,13 @@ MODELS_COST = {
     "gpt-3.5-turbo-instruct": {
         "input": 1.5,
         "output": 2
-    }
+    },
+    "gpt-4-0125-Preview": {
+        "input": 10,
+        "output": 30
+    },
+    "gpt-4-0613": {
+        "input": 30,
+        "output": 60
+    },
 }
