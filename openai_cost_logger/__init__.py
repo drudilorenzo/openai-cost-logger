05891fcfc06c88b9097cc03b422d2e42e0784eb0
Lorenzo Drudi <lori.drudi.11@gmail.com>
2024-04-27 15:44:59 +0200

    fix(docs): fix demo nb.

--- openai_cost_logger/__init__.py
+++ openai_cost_logger/__init__.py
@@ -2,4 +2,4 @@ from .openai_cost_logger import OpenAICostLogger
 from .constants import MODELS_COST, DEFAULT_LOG_PATH
 from .openai_cost_logger_viz import OpenAICostLoggerViz
 from .openai_cost_logger_utils import OpenAICostLoggerUtils
-print('imported openai_cost_logger')
+print('imported openai-cost-logger')
